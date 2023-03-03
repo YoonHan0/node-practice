63f08a7651b9ac6eb4135d4633c9fe338c2fb811
YoonHan0 <dbsgksdud12@naver.com>
2023-03-03 14:19:10 +0900

    node #3

---
+++ project-ex02/app-ex02.js
@@ -0,0 +1,12 @@
+/*
+    douzone-math npm 모듈 테스트(모듈 패키지: 로컬 설치)
+
+    로컬 패키지 배포
+    
+*/
+
+var math = require('douzone-math'); 
+
+console.log(math.PI);
+console.log(math.max(100, 200, 300, 400, 500, 105, 140));
+console.log(math.min(100, 200, 300, 400, 500, 105, 140));
