2039fdfee447332f20d4cd0fa13406e00ac8e7b3
YoonHan0 <dbsgksdud12@naver.com>
2023-03-03 17:55:13 +0900

    node #3

---
+++ helloweb-ex01/app02.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var port = 9090;
+var server = http.createServer(function(req, resp) { // 요청이 들어오면 실행되는 function
+    console.log(req);
+    
+    resp.writeHead(200, {
+        'Content-Type' : "text/html"
+    });
+    resp.end('<h1>Hello World!</h1>')
+});
+
+server.listen(port, function() {
+    console.log("http server running on " + port);
+});
--- project-ex02/app-ex02.js
+++ project-ex02/app-ex02.js
@@ -2,7 +2,8 @@
     douzone-math npm 모듈 테스트(모듈 패키지: 로컬 설치)
 
     로컬 패키지 배포
-    
+    $[project-ex02] npm i ../douzone-math
+    설치 후 사용
 */
 
 var math = require('douzone-math'); 
